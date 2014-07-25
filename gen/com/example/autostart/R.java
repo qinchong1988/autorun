caf1d41b114e2de4e2d4fe1f14adbda286d1ffb0
Chong Qin <qinchong1988@gmail.com>
2014-07-25 15:28:02 +0800

    不追踪gen目录

--- gen/com/example/autostart/R.java
+++ gen/com/example/autostart/R.java
@@ -24,14 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f080009;
         public static final int allow_bt=0x7f080000;
         public static final int cProgressBar=0x7f080004;
-        public static final int cb=0x7f080007;
+        public static final int cb=0x7f080008;
         public static final int forbid_bt=0x7f080001;
         public static final int go_bt=0x7f080003;
         public static final int img=0x7f080005;
         public static final int lv=0x7f080002;
+        public static final int receivers=0x7f080007;
         public static final int tv=0x7f080006;
     }
     public static final class layout {
